10a3285930141cca5cb7ac2d3f3b32d1511578be
Jordan Widdison <jordan.widdison@divvypay.com>
2022-12-07 12:36:37 -0700

    Day 7, Part 2

--- lib/advent_of_code/day_7.ex
+++ lib/advent_of_code/day_7.ex
@@ -6,8 +6,8 @@ defmodule AdventOfCode.Day7 do
   alias AdventOfCode.Inputs
 
   @type command :: String.t()
-  @type directory :: map
-  @type path :: String.t()
+  @type path :: list
+  @type sizes :: map
 
   @doc """
   Examples:
@@ -21,7 +21,7 @@ defmodule AdventOfCode.Day7 do
     commands = get_commands()
 
     commands
-    |> commands_to_file_tree()
+    |> commands_to_file_sizes()
     |> Enum.filter(fn {_path, size} -> size <= 100_000 end)
     |> Enum.reduce(0, fn {_path, size}, total_size -> total_size + size end)
   end
@@ -30,13 +30,18 @@ defmodule AdventOfCode.Day7 do
   Examples:
 
       iex> Day7.part_2()
-      0
+      12_785_886
 
   """
   @spec part_2() :: integer
   def part_2() do
-    _commands = get_commands()
-    0
+    commands = get_commands()
+
+    commands
+    |> commands_to_file_sizes()
+    |> Enum.filter(fn {_path, size} -> size > 8_381_165 end)
+    |> Enum.min_by(fn {_path, total_size} -> total_size end)
+    |> elem(1)
   end
 
   ################################################################################
@@ -50,63 +55,35 @@ defmodule AdventOfCode.Day7 do
     |> String.split("\n", trim: true)
   end
 
-  @spec commands_to_file_tree(list(command)) :: directory
-  defp commands_to_file_tree(commands) do
-    start_path = ["/"]
-    start_dir = %{"/" => %{}}
-    start_sizes = %{}
-
-    commands
-    |> Enum.reduce({start_path, start_dir, start_sizes, 1}, &handle_command/2)
-    |> elem(2)
+  @spec commands_to_file_sizes(list(command)) :: sizes
+  defp commands_to_file_sizes(commands) do
+    {_final_path, final_sizes} = Enum.reduce(commands, {["/"], %{}}, &handle_command/2)
+    final_sizes
   end
 
-  @spec handle_command(command, {path, directory, map, integer}) :: {path, directory, map, integer}
-  defp handle_command("$ cd /", {_path, directory, sizes, x}), do: {["/"], directory, sizes, x + 1}
+  @spec handle_command(command, {path, sizes}) :: {path, sizes}
+  defp handle_command("$ cd /", {_path, sizes}), do: {["/"], sizes}
+  defp handle_command("$ cd ..", {path, sizes}), do: {Enum.drop(path, -1), sizes}
+  defp handle_command("$ cd " <> new_folder, {path, sizes}), do: {path ++ [new_folder], sizes}
+  defp handle_command("$ ls", {path, sizes}), do: {path, sizes}
+  defp handle_command("dir " <> _dir_name, {path, sizes}), do: {path, sizes}
 
-  defp handle_command("$ cd ..", {path, directory, sizes, x}), do: {Enum.drop(path, -1), directory, sizes, x + 1}
-
-  defp handle_command("$ cd " <> new_folder, {path, directory, sizes, x}) do
-    new_path = path ++ [new_folder]
-
-    new_dir =
-      if get_in(directory, new_path) do
-        directory
-      else
-        put_in(directory, new_path, %{})
-      end
-
-    {new_path, new_dir, sizes, x + 1}
-  end
-
-  # do nothing
-  defp handle_command("$ ls", {path, directory, sizes, x}), do: {path, directory, sizes, x + 1}
-
-  # do nothing
-  defp handle_command("dir " <> _dir_name, {path, directory, sizes, x}), do: {path, directory, sizes, x + 1}
-
-  defp handle_command(file_with_size, {path, directory, sizes, x}) do
-    [file_size, file_name] = String.split(file_with_size, " ", trim: true)
+  defp handle_command(file_with_size, {path, sizes}) do
+    [file_size | _rest] = String.split(file_with_size, " ")
     {int_file_size, _rem} = Integer.parse(file_size)
-    path_to_file = path ++ [file_name]
 
-    new_dir = put_in(directory, path_to_file, int_file_size)
-
-    new_sizes = update_total_sizes(path, sizes, int_file_size, x)
-
-    {path, new_dir, new_sizes, x + 1}
+    {path, update_total_sizes(path, sizes, int_file_size)}
   end
 
-  defp update_total_sizes(["/"], sizes, int_file_size, _x) do
-    Map.update(sizes, "/", int_file_size, fn current_size -> current_size + int_file_size end)
-  end
+  @spec update_total_sizes(path, sizes, file_size :: integer) :: sizes
+  defp update_total_sizes([], sizes, _int_file_size), do: sizes
 
-  defp update_total_sizes(path, sizes, int_file_size, x) do
+  defp update_total_sizes(path, sizes, int_file_size) do
     path_to_size = Enum.join(path, "/")
     new_sizes = Map.update(sizes, path_to_size, int_file_size, fn current_size -> current_size + int_file_size end)
 
     path
     |> Enum.drop(-1)
-    |> update_total_sizes(new_sizes, int_file_size, x)
+    |> update_total_sizes(new_sizes, int_file_size)
   end
 end
