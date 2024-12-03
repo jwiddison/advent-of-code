5173d902d428dda6e9d399d01f0f85bd31bc01cd
Jordan Widdison <jordan.widdison@gmail.com>
2024-12-02 20:37:50 -0700

    2024, Day 1, Part 2

--- lib/advent_of_code/y24/day_1.ex
+++ lib/advent_of_code/y24/day_1.ex
@@ -8,25 +8,18 @@ defmodule AdventOfCode.Y24.Day1 do
   Examples:
 
       iex> Day1.part_1()
-      1
+      2_196_996
 
   """
   @spec part_1() :: integer
   def part_1() do
-    input = get_input()
+    {l, r} = get_lists()
 
-    {l, r} =
-      Enum.reduce(input, {[], []}, fn row, {left, right} ->
-        [l, r] = String.split(row, "   ")
-        new_left = [String.to_integer(l) | left] |> List.flatten()
-        new_right = [String.to_integer(r) | right] |> List.flatten()
+    sorted_left = Enum.sort(l)
+    sorted_right = Enum.sort(r)
 
-        {new_left, new_right}
-      end)
-
-    l
-    |> Enum.sort()
-    |> Enum.zip(Enum.sort(r))
+    sorted_left
+    |> Enum.zip(sorted_right)
     |> Enum.reduce(0, fn {x, y}, sum ->
       sum + abs(x - y)
     end)
@@ -36,12 +29,21 @@ defmodule AdventOfCode.Y24.Day1 do
   Examples:
 
       iex> Day1.part_2()
-      2
+      23_655_822
 
   """
   @spec part_2() :: integer
   def part_2() do
-    2
+    {l, r} = get_lists()
+
+    frequencies_map = Enum.frequencies(r)
+
+    Enum.reduce(l, 0, fn item, acc ->
+      case Map.get(frequencies_map, item) do
+        nil -> acc
+        frequency -> acc + item * frequency
+      end
+    end)
   end
 
   ################################################################################
@@ -53,5 +55,18 @@ defmodule AdventOfCode.Y24.Day1 do
     1
     |> Helpers.read_file("Y24")
     |> String.split("\n", trim: true)
+  end
+
+  @spec get_lists() :: tuple
+  defp get_lists() do
+    input = get_input()
+
+    Enum.reduce(input, {[], []}, fn row, {left, right} ->
+      [l, r] = String.split(row, "   ")
+      new_left = [String.to_integer(l) | left] |> List.flatten()
+      new_right = [String.to_integer(r) | right] |> List.flatten()
+
+      {new_left, new_right}
+    end)
   end
 end
